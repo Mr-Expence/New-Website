31f6b0f0eee0f3d596c48d486dc133039058402e
Mr_Expence <zedstarnight@gmail.com>
2026-05-17 18:18:23 +1000

    Made topbar a script

---
+++ header.js
@@ -0,0 +1,21 @@
+const topbar = `
+<nav class="topbar">
+    <a href="/" class="topbar-brand">
+      <img src="assets/cat_logo.png" alt="Mr_Expence logo">
+      Mr_Expence
+    </a>
+    <input type="checkbox" id="topbar-toggle" class="topbar-checkbox" aria-hidden="true">
+    <label for="topbar-toggle" class="topbar-toggle" aria-label="Toggle navigation menu">
+      <span class="topbar-icon topbar-icon-open" aria-hidden="true">dehaze</span>
+      <span class="topbar-icon topbar-icon-close" aria-hidden="true">close</span>
+    </label>
+    <div class="topbar-right">
+      <a href="/">Home</a>
+      <a href="/projects">Projects</a>
+      <a href="/games">Games</a>
+      <a href="/socials">Socials</a>
+      <a href="/problems">Problems</a>
+    </div>
+  </nav>
+`;
+document.body.insertAdjacentHTML("afterbegin", topbar);
